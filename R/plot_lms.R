0ee2e093eb3c72dbb900a2ea79099d723aaf7f40
Corey Kimzey <corey2kimzey@gmail.com>
2017-10-16 12:41:36 -0400

    This now plots the multivariate version of plot_lms

--- R/plot_lms.R
+++ R/plot_lms.R
@@ -22,13 +22,27 @@ plot_lms <- function(dataset, col_names, response_variable, multivariate = FALSE
   }else{
     response_col <- dataset[[response_variable]]
     predictor_col <- matrix(nrow = nrow(dataset), ncol = length(col_names))
+    summary_line = NULL
+    par(mfrow=c(ceiling(length(col_names)/2), 2))
     for(i in 1:length(col_names)){
     predictor_col[,i] <- dataset[[col_names[i]]]
     }
 
     line_needed <- lm(response_col ~ predictor_col)
     names(line_needed$coefficients)[2:(length(col_names)+1)] <- col_names
-    return(summary(line_needed))
+    for(j in 1:length(col_names)){
+      summary_line <- summary(line_needed)
+      plot(predictor_col[,j],
+           response_col,
+           ylab = response_variable,
+           xlab = col_names[j],
+           main = paste("Resting =", round(line_needed$coefficients[j+1], 2),
+                        col_names[j], "+", round(line_needed$coefficients[1],2)),
+           sub = paste("Coefficient P-value = ", round(summary_line$coefficients[j+1,4],3)))
+      abline(a = line_needed$coefficients[1], b = line_needed$coefficients[(j+1)] )
+
+
+    }
     }
 
 
