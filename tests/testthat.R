a6852828b63209407841baee3dcfd6755af1474e
Corey Kimzey <corey2kimzey@gmail.com>
2017-12-11 12:21:41 -0500

    Updated tests to match changes

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(heartrate)
 
 
-data("ckheartRate")
+data("tester")
 test_that("target_range works", {
-  expect_equal(unname(target_ranges(ckheartRate$`Resting Heart Rate`, time_data = ckheartRate$Day)[1,2]), 77)
+  expect_equal(unname(target_ranges(tester$`Resting Heart Rate`, time_data = tester$Day)[1,2]), 77)
 })
